c2f9acc905258a73aeae297f89b64113cd3890d6
Alec Blance <17378596+AlecBlance@users.noreply.github.com>
2023-11-21 12:01:11 +0800

    chore: 👍 add image source

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "mir-s3-cdn-cf.behance.net",
+        port: "",
+        pathname: "/projects/**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
