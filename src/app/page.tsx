98797164964af438948339d540f40f7fd82a838f
Alec Blance <17378596+AlecBlance@users.noreply.github.com>
2023-11-21 12:02:42 +0800

    refactor: ✌ redundant padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
+import ProjectsContainer from "@/components/projects-container/projects-container";
 import SearchContainer from "@/components/search-container/search-container";
 
 export default function Home() {
   return (
-    <div className="px-3 py-2">
-      <div className="max-w-5xl mx-auto pt-4">
+    <div className="px-3 py-6">
+      <div className="max-w-5xl mx-auto space-y-4">
         <SearchContainer />
+        <ProjectsContainer />
       </div>
     </div>
   );
