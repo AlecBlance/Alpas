b19a2973f4426c0d5486288dab5c5fb42a7af6c6
Alec Blance <17378596+AlecBlance@users.noreply.github.com>
2023-11-23 12:09:56 +0800

    style: 😎 auto grown main content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import SearchContainer from "@/components/search-container/search-container";
 
 export default function Home() {
   return (
-    <div className="px-3 py-6">
+    <div className="px-3 py-6 flex-grow">
       <div className="max-w-5xl mx-auto space-y-6">
         <SearchContainer />
         <ProjectsContainer />
